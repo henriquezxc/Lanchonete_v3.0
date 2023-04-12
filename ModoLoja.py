9609c4bfb13221b4fdd58351296c3684981be30b
henriquezxc <henriqueverisimop@gmail.com>
2023-04-12 18:20:59 -0300

    Inicio do projeto

---
+++ ModoLoja.py
@@ -0,0 +1,23 @@
+# Aqui esta todo o algoritmo do modo loja, onde fica armazenado todos os dados de compra
+
+print('''
+#################################################################
+VOCÊ ESTÁ NO MODO LOJA, AQUI ESTARÁ TODOS OS DADOS DE SUA EMPRESA 
+#################################################################
+''')
+print('''
+1 - LISTAR TODOS OS PRODUTOS VENDIDOS
+2 - MOSTRAR QUANTIDADE DE CADA PRODUTO VENDIDO
+3 - VALOR BRUTO DE TODOS OS PRODUTOS VENDIDOS 
+''')
+
+Entrada_Usuario = str(input('DIGITE UMA OPÇÃO: '))
+
+def Selecao_Um():
+    pass
+
+def Selecao_Dois():
+    pass
+
+def Selecao_Tres():
+    pass
---
+++ temporario.py
@@ -0,0 +1,7 @@
+lista = ['feijao','arroz','macarrao','banana']
+#            0       1        2          3
+
+
+lista.remove(len(lista[1]))
+
+print(lista)
