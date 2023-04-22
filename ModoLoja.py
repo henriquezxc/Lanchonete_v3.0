c54a7926791dd18487d3581265f7fe22fcbb35a7
henriquezxc <henriqueverisimop@gmail.com>
2023-04-22 19:05:41 -0300

    Alteramos todo o algoritmo do modo 'Loja.py' adicionei laço para repetir varias condições

--- ModoLoja.py
+++ ModoLoja.py
@@ -1,26 +1,12 @@
 # Aqui esta todo o algoritmo do modo loja, onde fica armazenado todos os dados de compra
 from Cores import *
-print(f'''
-#################################################################
-{amarelo}VOCÊ ESTÁ NO MODO LOJA, AQUI ESTARÁ TODOS OS DADOS DE SUA EMPRESA{defalt} 
-#################################################################
-''')
-print(f'''{verde2}
-1 - LISTAR TODOS OS PRODUTOS VENDIDOS NA ORDEM DE COMPRA 
-2 - MOSTRAR QUANTIDADE DE CADA PRODUTO VENDIDO
-3 - VALOR BRUTO DE TODOS OS PRODUTOS VENDIDOS #Em Desenvolvimento...
-0 - LIMPAR TODA A LISTA DE PRODUTOS POR COMPLETO
-{defalt}''')
-# Usuario seleciona a opção desejada conforme pede acima. 
-Entrada_Usuario = str(input('DIGITE UMA OPÇÃO: '))
+
 # Ao selecionar a opção 1 o codigo a seguir e executado.
 def Selecao_Um():
     with open('Dados.txt') as arquivo:
         i = arquivo.read()
         print(i)
 # Ao usuario digitar a string '1' irá acionar o def Selecao_Um(): criado acima.
-if Entrada_Usuario == '1':
-    Selecao_Um()
 
 # Ao selecionar a opção 2 o codigo a seguir e executado.
 def Selecao_Dois():
@@ -32,17 +18,6 @@ def Selecao_Dois():
     print (Lista_Temp.count ('Torrada Simples R$ 2.49\n'),'- Torrada Simples')
     print (Lista_Temp.count ('Refrigerante R$ 2.00\n'),'- Refrigentes')
 # Ao usuario digitar a string '2' irá acionar o def Selecao_Dois(): criado acima.
-if Entrada_Usuario == '2':
-    Selecao_Dois()
-
-# Ao selecionar a opção 3 o codigo a seguir e executado.
-def Selecao_Tres():
-    pass
-
-
-def Selecao_Quatro():
-    pass
-
 
 # Ao selecionar a opção 0 o codigo a seguir e executado.
 def Selecao_Zero():
@@ -73,6 +48,7 @@ def Selecao_Zero():
             if contador == 4:
                 print(f'{vermelho2}VOCÊ ERROU A COMBINAÇÃO MAIS DE 5 VEZES! POR ISSO O PROGRAMA ENCERRARÁ.{defalt}')
                 # Comando usado para finalizar o laço e encerrar o programa.
+                sair_Menu()
                 break
             # Usado no laço para imprimir a mensagem de senha ínvalida e mais alguns comandos mensionados abaixo.
             else:
@@ -85,7 +61,64 @@ def Selecao_Zero():
 
     if Entrada_Usuario_Zero == 'NAO':
         print(f'{vermelho2}COMANDO CANCELADO{defalt}')
-
 # Ao usuario digitar a string '0' irá acionar o def Selecao_Zero(): criado acima.
-if Entrada_Usuario == '0':
-    Selecao_Zero()
+
+def menu_de_opcoes():
+    print(f'''
+    #################################################################
+    {amarelo}VOCÊ ESTÁ NO MODO LOJA, AQUI ESTARÁ TODOS OS DADOS DE SUA EMPRESA{defalt} 
+    #################################################################
+    ''')
+    print(f'''{verde2}
+    1 - LISTAR TODOS OS PRODUTOS VENDIDOS NA ORDEM DE COMPRA 
+    2 - MOSTRAR QUANTIDADE DE CADA PRODUTO VENDIDO
+    3 - VALOR BRUTO DE TODOS OS PRODUTOS VENDIDOS #Em Desenvolvimento...
+    0 - LIMPAR TODA A LISTA DE PRODUTOS POR COMPLETO
+    {defalt}''')
+
+def Menu_de_condicoes(): 
+    while True:
+    
+        Entrada_Usuario = str(input('DIGITE UMA OPÇÃO: '))
+        if Entrada_Usuario == '1':
+            Selecao_Um()
+            while True:
+                Entrada_temp_1 = str(input('Parar ou continuar: ').upper())
+                if Entrada_temp_1 == 'PARAR':
+                    sair_Menu()
+                elif Entrada_temp_1 ==  'CONTINUAR':
+                    menu_de_opcoes()
+                    break
+                else:
+                    print(f'{vermelho2}Entrada Invalida!{defalt}')
+
+        elif  Entrada_Usuario == '2':
+            Selecao_Dois()
+            while True:
+                Entrada_temp_2 = str(input('Parar ou Continuar: ').upper())
+                if Entrada_temp_2 == 'PARAR':
+                    sair_Menu()
+                elif Entrada_temp_2 ==  'CONTINUAR':
+                    menu_de_opcoes()
+                    break
+                else:
+                    print(f'{vermelho2}Entrada Invalida!{defalt}')
+
+        elif Entrada_Usuario == '0':
+            Selecao_Zero()
+            while True:
+                Entrada_temp_3 = str(input('Parar ou Continuar: ').upper())
+                if Entrada_temp_3 == 'PARAR':
+                    sair_Menu()
+                elif Entrada_temp_3 ==  'CONTINUAR':
+                    menu_de_opcoes()
+                    break
+                else:
+                    print(f'{vermelho2}Entrada Invalida!{defalt}')
+
+def sair_Menu():
+    print(f'{vermelho2}SEU PROGRAMA FOI ENCERRADO.{defalt}')
+    exit()
+
+menu_de_opcoes()
+Menu_de_condicoes()
